3ee0897ca38ce76e964847f5a28177515c865fe1
MAURICIO SHIGUEMITSU KAMADO IKEDA <mauricioikeda17@gmail.com>
2026-05-12 17:21:02 -0300

    Correcaozinha XD

---
+++ Auth.Infrastructure/Migrations/20260512195627_pobrandifazmigrationkarai.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Auth.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class pobrandifazmigrationkarai : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthUsers_Email",
+                table: "AuthUsers",
+                column: "Email",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AuthUsers_Email",
+                table: "AuthUsers");
+        }
+    }
+}
