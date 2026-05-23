72e90bbefde5610a3e922eeccbedff8909075133
MAURICIO SHIGUEMITSU KAMADO IKEDA <mauricioikeda17@gmail.com>
2026-05-23 17:50:17 -0300

    Adicionando include com retorno da entidade

--- Generic.Application/Crud/Interface/IUseCaseCrudCollection.cs
+++ Generic.Application/Crud/Interface/IUseCaseCrudCollection.cs
@@ -2,10 +2,11 @@ using Generic.Domain.Entities;
 
 namespace Generic.Application.Crud.Interface;
 
-public interface IUseCaseCrudCollection<TEntity, in TCommand> : IUseCaseQueryCollection<TEntity>
+public interface IUseCaseCrudCollection<TEntity, TCommand> : IUseCaseQueryCollection<TEntity>
     where TEntity : BaseEntity
 {
     OperationResult Include(TCommand command);
+    OperationResult<TCommand> IncludeWithResult(TCommand command);
     OperationResult Update(TCommand command);
     OperationResult Delete(long id);
 }
